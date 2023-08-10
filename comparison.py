0566c63804d1ced6fd25b0b909d7aebaf90b8324
Ivan Kasibwa <ivanagesa@gmail.com>
2023-08-10 19:49:51 +0300

    Comparison operators

---
+++ comparison.py
@@ -0,0 +1,31 @@
+# Comparison: 7 + 5 >= 10
+comparison_1 = 7 + 5 >= 10
+print("7 + 5 >= 10:", comparison_1)
+
+# Comparison: "Anil" > "anil"
+comparison_2 = "Anil" > "anil"
+print("\"Anil\" > \"anil\":", comparison_2)
+
+# Comparison: 25 % 3 == 2
+comparison_3 = 25 % 3 == 2
+print("25 % 3 == 2:", comparison_3)
+
+# Comparison: 23 // 7 >= 9
+comparison_4 = 23 // 7 >= 9
+print("23 // 7 >= 9:", comparison_4)
+
+# Comparison: 7 + 3 % 2 != True
+comparison_5 = 7 + 3 % 2 != True
+print("7 + 3 % 2 != True:", comparison_5)
+
+# Comparison: False == False
+comparison_6 = False == False
+print("False == False:", comparison_6)
+
+# Comparison: True != False
+comparison_7 = True != False
+print("True != False:", comparison_7)
+
+# Comparison: 29 == 89 % 30
+comparison_8 = 29 == 89 % 30
+print("29 == 89 % 30:", comparison_8)
